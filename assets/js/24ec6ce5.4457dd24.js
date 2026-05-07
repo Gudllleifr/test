5c373cb0bac48040d5cfddd2545805eda21c4737
Gudllleifr <Gudllleifr@users.noreply.github.com>
2026-05-07 06:05:30 +0000

    deploy: d4e015f931e37c9bf54c43ce18d6496bdad59a01

---
+++ assets/js/24ec6ce5.4457dd24.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[774],{3336:e=>{e.exports=JSON.parse('{"tags":[{"label":"\u041e\u0444\u043e\u0440\u043c\u043b\u0435\u043d\u0438\u0435","permalink":"/test/style-guide/tags/\u043e\u0444\u043e\u0440\u043c\u043b\u0435\u043d\u0438\u0435","count":1}]}')}}]);
